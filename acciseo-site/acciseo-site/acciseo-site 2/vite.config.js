56a895ae0c6fa5d7e988fd3c4cf35e61dda2d534
yacinebcontact-creator <yacineb.contact@gmail.com>
2026-09-06 20:35:35 +0200

    Add files via upload

---
+++ acciseo-site/acciseo-site/acciseo-site 2/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: { rollupOptions: { input: { main: resolve(__dirname, 'index.html'), controle: resolve(__dirname, 'controle.html') } } },
+})
